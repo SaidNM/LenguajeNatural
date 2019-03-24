3ca3cb5e4a92625152dbee7c11f93245d7f8ec5a
SaidNM <saidnm044@hotmail.com>
2019-03-23 23:57:45 -0600

    matriz idf tf, con diccionarios

--- matriz/funciones.py
+++ matriz/funciones.py
@@ -3,42 +3,55 @@ import re
 import math
 
 def extract_words(sentence):
-    words = re.sub("[^\w]", " ",  sentence).split()
-    words_cleaned = [w.lower() for w in words]
-    return words_cleaned
+    words = re.sub("[^\w]", " ",  sentence).split() 
+    return words
+
+def create_Vocabulary(words,dic_words):
+	words = sorted(list(set(words)))
+	for w in words:
+		if(w in dic_words):
+			dic_words[w] = dic_words[w]+1
+		else:
+			dic_words.setdefault(w,1)
+	return dic_words
 
 def tokenize_sentences(sentences):
-	words=[]
+	vocabulary = {}
+	aux_vocabulary = {}
 	file = open ("bagWords.txt","w")
 	for sentence in sentences:
-		aux_words = extract_words(sentence)
-		words.extend(aux_words)
-
-	words=sorted(list(set(words)))
-	file.write(str(words))
+		aux_vocabulary = create_Vocabulary(sentence,aux_vocabulary)
+	
+	vocabulary.update(aux_vocabulary)
+	file.write(str(vocabulary))
 	file.close()
-	return words
+	return vocabulary
 
-def matrizTF(bag,datos):
-	mTF = [];
-	for dato in datos:
-		v = vectorTF(bag,dato)
-		mTF.append(v)
-	return mTF
+def list_dictionary(list_text):
+	list_dico = [];
+	for text in list_text:
+		dictionary = {}
+		for word in text:
+			if(word not in dictionary):
+				dictionary.setdefault(word,text.count(word))
+		list_dico.append(dictionary)
+	return list_dico
 
-def vectorTF(bag,vector):
-	vTF = []
-	for word in bag:
-		x = vector.count(word)
-		vTF.append(x)
-	return vTF
-
-def showMatriz(bag,matriz):
+def showMatriz(vocabulary,list_dictionary):
 	file = open("resultado.txt","w")
-	b = np.array([bag,matriz[0],matriz[1]])
-	file.write(str(np.transpose(b)))
-	print(np.transpose(b))
+	matriz = []
+	print("Palabra\tD1\tD2\tD3")
+	print("")
+	for word in vocabulary:
+		print(word,end=" ")
+		for dico in list_dictionary:
+			if(word not in dico):
+				print ("\t 0",end=" ")
+			else:
+				print("\t "+str(dico[word]),end=" ")
+		print()
 	file.close()
+
 # Medicion de la similitud
 """
 def euclidean_distance(vector1,vector2):
@@ -46,9 +59,18 @@ def euclidean_distance(vector1,vector2):
 	for i in range(len(vector1)):
 		distance += (vector2[i]-vector1[i])**2
 	return math.sqrt(distance)
+"""
+def create_list(vocabulary,dictionary):
+	data = []
+	for word in vocabulary:
+		if(word in dictionary):
+			data.append(dictionary[word])
+		else:
+			data.append(0)
+	return data
+
 def cosine_similarity(vector1,vector2):
 	vectorA = np.array(vector1)
 	vectorB = np.array(vector2)
 	cosine = (np.dot(vector1,vector2))/((math.sqrt(np.dot(vector1,vector1))) * np.sqrt((np.dot(vector2,vector2))))
 	return cosine
-"""
--- matriz/mainMatriz.py
+++ matriz/mainMatriz.py
@@ -4,29 +4,48 @@ def main():
 	#Abrimos el archivo uno
 	text1=open("ejemplo1.txt","r")
 	sentences_text1 = text1.read().lower()
+	sentences_text1 = fnMatriz.extract_words(sentences_text1)
 	#Abrimos el archivo dos
 	text2=open("ejemplo2.txt","r")
 	sentences_text2 = text2.read().lower()
+	sentences_text2 = fnMatriz.extract_words(sentences_text2)
 	#Abrimos el archivo tres
 	text3 = open("ejemplo.txt","r")
 	sentences_text3 = text3.read().lower()
-	#Creamos la bolsa de palabras
-	bag = fnMatriz.tokenize_sentences([sentences_text1,sentences_text2])
-	print("----- palabras -----")
-	print(bag)
-	#Obtenemos los vectores TF de cada texto
-	matriz = fnMatriz.matrizTF(bag,[sentences_text1,sentences_text2])
+	sentences_text3 = fnMatriz.extract_words(sentences_text3)
+	#Creamos nuestro vocabulario (Diccionario key = palabra : value= idf)
+	vocabulary = fnMatriz.tokenize_sentences([sentences_text1,sentences_text2,sentences_text3])
+	print("----- Vocabulario -----")
+	print(vocabulary)
+
+	#Obtenemos los diciconario de cada cada texto (Diccionario key = palabra : value = tf)
+	matrix = fnMatriz.list_dictionary([sentences_text1,sentences_text2,sentences_text3])
 
 	print("--- matriz TF ---")
-	fnMatriz.showMatriz(bag,matriz)
+	fnMatriz.showMatriz(vocabulary,matrix)
+	
 	print("-----Similitud-----")
 	#Calculamos la similitud euclidiana
 	#simEuclidean = euclidean_distance(bagofWords_text1,bagofWords_text2)
 	#print("Distancia euclidiana: " +str(simEuclidean))
+	
 	#Calculamos la similitud coseno
-	#simCos = cosine_similarity(bagofWords_text1,bagofWords_text2)
-	#print ("Similitud coseno: " + str(simCos))
+	
+	#Enlistamos los valores de los tres diccionarios
+	list_text1 = fnMatriz.create_list(vocabulary,matrix[0])
+	list_text2 = fnMatriz.create_list(vocabulary,matrix[1])
+	list_text3 = fnMatriz.create_list(vocabulary,matrix[2])
+
+
+	simCos1_2 = fnMatriz.cosine_similarity(list_text1,list_text2)
+	simCos1_3 = fnMatriz.cosine_similarity(list_text1,list_text3)
+	simCos2_3 = fnMatriz.cosine_similarity(list_text2,list_text3)
+	print ("Similitud coseno texto1-texto2: " + str(simCos1_2))
+	print ("Similitud coseno texto1-texto3: " + str(simCos1_3))
+	print ("Similitud coseno texto2-texto3: " + str(simCos2_3))
+	
 	text1.close()
 	text2.close()
 	text3.close()
+
 main()
